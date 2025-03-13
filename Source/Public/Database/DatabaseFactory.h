913e30cbfac9afa48a8fabf979955fc22afe7e40
✪Ohmcodes™✪ <ohmcodes@gmail.com>
2025-03-13 12:34:26 -0700

    Auto push after build

--- Source/Public/Database/DatabaseFactory.h
+++ Source/Public/Database/DatabaseFactory.h
@@ -4,7 +4,7 @@
 #pragma once
 
 #include "MySQLConnector.h"
-#include "SQLiteConnector.h"
+//#include "SQLiteConnector.h"
 #include <json.hpp>
 #include <memory>
 
@@ -13,29 +13,31 @@ class DatabaseFactory
 public:
 	static std::unique_ptr<IDatabaseConnector> createConnector(const nlohmann::json config)
 	{
+		int sslMode = config.value("MysqlSSLMode", -1);
+		std::string tlsVersion = config.value("MysqlTLSVersion", "");
+
+		return std::make_unique<MySQLConnector>(
+			config["Host"],
+			config["User"],
+			config["Password"],
+			config["Database"],
+			config["Port"],
+			sslMode,
+			tlsVersion
+		);
+
 		if (config.value("UseMySQL", true))
 		{	
-			int sslMode = config.value("MysqlSSLMode", -1);
-			std::string tlsVersion = config.value("MysqlTLSVersion", "");
-
-			return std::make_unique<MySQLConnector>(
-				config["Host"],
-				config["User"],
-				config["Password"],
-				config["Database"],
-				config["Port"],
-				sslMode,
-				tlsVersion
-			);
+			
 		}
 		else
 		{
-			Log::GetLog()->warn("Goes here");
+			/*Log::GetLog()->warn("Goes here");
 			std::string default_path = AsaApi::Tools::GetCurrentDir() + "/ArkApi/Plugins/" + PROJECT_NAME + "/" + ".db";
 			std::string sqlitePath = config.value("SQLiteDatabasePath","");
 			std::string db_path = (sqlitePath == "") ? default_path : sqlitePath;
 
-			return std::make_unique<SQLiteConnector>(db_path);
+			return std::make_unique<SQLiteConnector>(db_path);*/
 		}
 	}
 };
